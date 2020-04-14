f8fb6b02963b6be022a9a5e1fad5a384d9fcffa3
Sonja Parsell <hello@sparsell.com>
2020-04-14 12:06:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+#keys of grade levels. The value of each key will be an array of student names
+
+
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name
+
+  end
+
+  def grade(grade)
+    return roster[grade].values
+  end
+
+  def sort(grade)
+    return roster[grade].sort
+  end
+
+end
